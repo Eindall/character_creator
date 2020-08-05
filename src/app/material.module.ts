81b1db3a1b479dc8917718108ae2ad187c6a1ece
Eindall <marquisthomas4369@gmail.com>
2020-08-05 11:07:21 +0200

    Imports from material angular

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserAnimationsModule,
         MatButtonModule,
         MatIconModule,
+        MatMenuModule,
         MatToolbarModule
     ],
     exports: [
@@ -21,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserAnimationsModule,
         MatButtonModule,
         MatIconModule,
+        MatMenuModule,
         MatToolbarModule
     ],
     providers: []
