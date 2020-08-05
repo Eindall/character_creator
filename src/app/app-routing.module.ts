9991f1e0cf70e73c704e91caf736b87bc2cc8af8
Eindall <marquisthomas4369@gmail.com>
2020-08-05 11:44:51 +0200

    Created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PfSheetCreatorComponent } from './pathfinder/sheet-creator/sheet-creator.component';
+import { PfSheetDetailsComponent } from './pathfinder/sheet-details/sheet-details.component';
+import { NhSheetCreatorComponent } from './naheulbeuk/sheet-creator/sheet-creator.component';
+import { NhSheetDetailsComponent } from './naheulbeuk/sheet-details/sheet-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'pathfinder/create',
+    component: PfSheetCreatorComponent
+  },
+  {
+    path: 'pathfinder/details/:id',
+    component: PfSheetDetailsComponent
+  },
+  {
+    path: 'naheulbeuk/create',
+    component: NhSheetCreatorComponent
+  },
+  {
+    path: 'naheulbeuk/details/:id',
+    component: NhSheetDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
